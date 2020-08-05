c4f5bb2913d499468e483740c5c6919e3507ae5e
eze8789 <ezequiel.reyna@gmail.com>
2020-08-05 17:55:37 -0300

    Remove capitalization andd punctuation from the error and remove some comments

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -9,12 +9,10 @@ func Distance(a, b string) (int, error) {
 
 	aRune, bRune := []rune(a), []rune(b)
 
-	// If not the same lenght exit
 	if len(aRune) != len(bRune) {
-		return 0, errors.New("The DNA samples have different lenght")
+		return 0, errors.New("the dna samples have different lenght")
 	}
 
-	// Initialize variable to count distance
 	var c = 0
 
 	for i := range aRune {
