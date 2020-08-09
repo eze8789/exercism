130b2f25ba3edb5a4977ed730eca1800d604cc80
eze8789 <ezequiel.reyna@gmail.com>
2020-08-09 01:05:01 -0300

    Change scrabble-score exercise to compare runes using Unicode

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,37 +1,27 @@
 package scrabble
 
 import (
-	"strings"
 	"unicode"
 )
-
-const onePoints = "A, E, I, O, U, L, N, R, S, T"
-const twoPoints = "D, G"
-const threePoints = "B, C, M, P"
-const fourPoints = "F, H, V, W, Y"
-const fivePoints = "K"
-const eightPoints = "J, X"
-const tenPoints = "Q, Z"
 
 // Score calculate how much point a word sum in scrabble game
 func Score(word string) int {
 	var c int
 	for _, r := range word {
-		r = unicode.ToUpper(r)
-		switch {
-		case strings.ContainsRune(onePoints, r):
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
 			c++
-		case strings.ContainsRune(twoPoints, r):
+		case 'D', 'G':
 			c += 2
-		case strings.ContainsRune(threePoints, r):
+		case 'B', 'C', 'M', 'P':
 			c += 3
-		case strings.ContainsRune(fourPoints, r):
+		case 'F', 'H', 'V', 'W', 'Y':
 			c += 4
-		case strings.ContainsRune(fivePoints, r):
+		case 'K':
 			c += 5
-		case strings.ContainsRune(eightPoints, r):
+		case 'J', 'X':
 			c += 8
-		case strings.ContainsRune(tenPoints, r):
+		case 'Q', 'Z':
 			c += 10
 		}
 	}
